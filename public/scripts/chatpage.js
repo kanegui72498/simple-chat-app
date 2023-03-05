b43cf6993ee1d43b4ff8be112a32bc92e0110895
Kane Gui <kgui72498@gmail.com>
2023-03-05 17:28:17 -0500

    Added support for nicknames and improved messaging
    *index.html*
    - removed the script inside html and moved to external file
    - renamed some of the ids for better readability
    
    *server.js*
    - created array for all users connected using socket ids to store usernames
    - added functionality to broadcast when user connects
    - added functionality to broadcast when user disconnects
    
    *index.css*
    - fixed some of the id names in the styling
    
    *chatpage.js*
    - added support for creating a username
    - renamed some variables for better readability
    - added event handling for when a user connects or disconnects
    - Improved appending messages to show "You:..." for yourself
    - Created helper function for better reusability of code

--- public/scripts/chatpage.js
+++ public/scripts/chatpage.js
@@ -1,19 +1,43 @@
 var socket = io();
       
-var form = document.getElementById('form');
-var input = document.getElementById('input');
+var chatBox = document.getElementById('chat-box');
+var message = document.getElementById('message');
+var messageList = document.getElementById('message-list');
 
-form.addEventListener('submit', function(e) {
-e.preventDefault();
-if (input.value) {
-socket.emit('chat message', input.value);
-input.value = '';
+//ask for username
+const username = prompt('Enter username:');
+messageList.append('You joined.'); //show that you have connected to the chat
+socket.emit('user created', username)
+
+//create event listener for sending message
+chatBox.addEventListener('submit', function(e) {
+    e.preventDefault(); //prevents page from refreshing after sending message
+    if (message.value) {
+        socket.emit('chat message', message.value); //send message (as long as not empty) to the server
+        messageList.append(`You: ${message.value}`) //show your own message
+        message.value = ''; //empty out the message box
+    }
+});
+
+//event handling for 'chat message'
+socket.on('chat message', function(messageData) {
+    sendMessage(`${messageData.username}: ${messageData.message}`);
+});
+
+//event handling for 'user connected'
+socket.on('user connected', username => {
+   sendMessage(`${username} has connected.`);
+});
+
+//event handling for 'user disconnected'
+socket.on('user disconnected', (username) => {
+    sendMessage(`${username} has disconnected`);
+});
+
+//helper function for creating and appending message
+function sendMessage(message) {
+    var messageToSend = document.createElement('li');
+    messageToSend.textContent = message;
+    messageList.appendChild(messageToSend);
+    window.scrollTo(0, document.body.scrollHeight);
 }
-});
-
-socket.on('chat message', function(msg) {
-var item = document.createElement('li');
-item.textContent = msg;
-messages.appendChild(item);
-window.scrollTo(0, document.body.scrollHeight);
-});
--- server.js
+++ server.js
@@ -5,19 +5,32 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const usersConnected = []; //empty array of all users connected
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+//when user loads website create a socket
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  //On username creation inform all other connected sockets
+  socket.on('user created', (username) => {
+    usersConnected[socket.id] = username; //every socket has unique id, set the username to array
+    socket.broadcast.emit('user connected', username)
+  })
+
+  //On socket disconnect inform all other connected sockets
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    if(usersConnected[socket.id]) { // check if user actually existed to avoid null state
+      socket.broadcast.emit('user disconnected', usersConnected[socket.id]);
+    }
   });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+
+  //On chat message, send the message to all other sockets connected
+  socket.on('chat message', (message) => {
+    socket.broadcast.emit('chat message', message);
   });
 });
 
