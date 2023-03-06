3e0d4f3c7b570206c7ab4797d482810df25a0055
Kane Gui <kgui72498@gmail.com>
2023-03-05 22:09:08 -0500

    added "{user} is typing" functionality
    *index.html*
    - added div to hold the list of users that are typing
    
    *server.js*
    - changed usersConnected into a object instead of array because socket.io has trouble emitting array objects
    - created usersTyping object to hold a list of all users currently typing
    - storing user object instead of just the name to provide an easy way to store user data if needed
    - on disconnect remove the user from userTyping list
    - after sending a message remove user from userTyping list and update other clients
    - created 'user is typing' and 'user is not typing' handlers
    - created helper function to get the current user by the current socketId
    
    *chatpage.js*
    - added variable for the usersTyping <div>
    - created a user object, which currently only has a 'name' property
    - fixed the way messages were being appended
    - added event listener for 'keyup', which emits 'user is typing' and 'user not typing'
    - added event handlign for 'users typing' to display all other users typing
    - created function to display users that are typing

--- public/scripts/chatpage.js
+++ public/scripts/chatpage.js
@@ -3,35 +3,56 @@ var socket = io();
 var chatBox = document.getElementById('chat-box');
 var message = document.getElementById('message');
 var messageList = document.getElementById('message-list');
+var usersTyping = document.getElementById('users-typing');
 
 //ask for username
 const username = prompt('Enter username:');
-messageList.append('You joined.'); //show that you have connected to the chat
-socket.emit('user created', username)
+
+//create user object
+let user = {
+    name: (username) ? username : 'Anonymous',
+}
+
+sendMessage('You joined.'); //show that you have connected to the chat
+socket.emit('user created', user)
 
 //create event listener for sending message
 chatBox.addEventListener('submit', function(e) {
     e.preventDefault(); //prevents page from refreshing after sending message
     if (message.value) {
         socket.emit('chat message', message.value); //send message (as long as not empty) to the server
-        messageList.append(`You: ${message.value}`) //show your own message
+        sendMessage(`You: ${message.value}`) //show your own message
         message.value = ''; //empty out the message box
     }
 });
 
-//event handling for 'chat message'
+//create event listener for when a user is typing
+message.addEventListener('keyup', function(e) {
+    if(message.value) {
+        socket.emit('user is typing'); //tell the server that this user is typing
+    } else {
+        socket.emit('user not typing'); //if input is tell the server this user is no longer typing
+    }
+});
+
+//event handling for other users sending messages
 socket.on('chat message', function(messageData) {
     sendMessage(`${messageData.username}: ${messageData.message}`);
 });
 
-//event handling for 'user connected'
+//event handling for other users connecting
 socket.on('user connected', username => {
-   sendMessage(`${username} has connected.`);
+    sendMessage(`${username} has connected.`);
 });
 
-//event handling for 'user disconnected'
+//event handling for other users disconnectiong
 socket.on('user disconnected', (username) => {
     sendMessage(`${username} has disconnected`);
+});
+
+//event handling for displaying all users typing
+socket.on('users typing', (users) => {
+    displayUsersTyping(users);
 });
 
 //helper function for creating and appending message
@@ -40,4 +61,38 @@ function sendMessage(message) {
     messageToSend.textContent = message;
     messageList.appendChild(messageToSend);
     window.scrollTo(0, document.body.scrollHeight);
+}
+
+//helper function to display other users typing
+function displayUsersTyping(users) {
+    var numUsersTyping = Object.keys(users).length;
+    const usernames = Object.values(users);
+
+    // current user should not be included in number of other users typing
+    if(usernames.includes(user.name)) { 
+        numUsersTyping--;
+    } 
+    
+    if(numUsersTyping == 1) {
+        let username = (usernames[0] != user.name) ? usernames[0] : usernames[1]; //display the user that is not the current one
+        usersTyping.innerText = `${username} is typing...`;
+    } else if (numUsersTyping == 0) {
+        usersTyping.innerText = '';
+    } else {
+        let message = '';
+        for(let i = 0; i < usernames.length; i++){
+            //on the last user concat 'and' instead
+            if(i == usernames.length - 1){
+               message += `and ${usernames[i]} `;
+               break;
+            }
+
+            //display if it does not match the current user
+            if(usernames[i] != user.name) {
+                message += `${usernames[i]}, `
+            }
+        }
+        message += 'are typing...'
+        usersTyping.innerText = message;
+    }
 }
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const usersConnected = []; //empty array of all users connected
+const usersConnected = {}; //create mapping for all users that are connected
+const usersTyping = {}; //create mapping for all users that are typing
 
 app.use(express.static(__dirname + '/public'));
 
@@ -16,24 +17,49 @@ app.get('/', (req, res) => {
 //when user loads website create a socket
 io.on('connection', (socket) => {
   //On username creation inform all other connected sockets
-  socket.on('user created', (username) => {
-    usersConnected[socket.id] = username; //every socket has unique id, set the username to array
-    socket.broadcast.emit('user connected', username)
+  socket.on('user created', (user) => {
+    usersConnected[socket.id] = user; //every socket has unique id, set the username to array
+    socket.broadcast.emit('user connected', user.name)
   })
 
   //On socket disconnect inform all other connected sockets
   socket.on('disconnect', () => {
-    if(usersConnected[socket.id]) { // check if user actually existed to avoid null state
-      socket.broadcast.emit('user disconnected', usersConnected[socket.id]);
+    if(currentUser(socket.id)) { // check to see user exists
+      socket.broadcast.emit('user disconnected', currentUser(socket.id).name);
+      delete currentUser(socket.id); //remove the user from the list of connected users
+      delete usersTyping[socket.id]; //user can no longer be typing if they are not connected
     }
   });
 
   //On chat message, send the message to all other sockets connected
   socket.on('chat message', (message) => {
-    socket.broadcast.emit('chat message', message);
+    if(currentUser(socket.id)){
+      socket.broadcast.emit('chat message', {message: message, username: currentUser(socket.id).name});
+      delete usersTyping[socket.id]; //user is no longer typing after sending message
+      socket.broadcast.emit('users typing', usersTyping); //send updated list of users typing to other sockets
+    }
   });
+
+  //On some user typing, inform all other sockets connected
+  socket.on('user is typing', () => {
+    if(currentUser(socket.id)) { //check to see user exists
+      usersTyping[socket.id] = currentUser(socket.id).name; //add current user to the list of users typing
+      socket.broadcast.emit('users typing', usersTyping);
+    }
+  });
+
+  //On some user no longer typing, stop informing all other sockets
+  socket.on('user not typing', () => {
+    delete usersTyping[socket.id];
+    socket.broadcast.emit('users typing', usersTyping);
+  })
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+//helper function to get the current user
+function currentUser(socketId){
+  return usersConnected[socketId];
+}
